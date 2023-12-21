b5469d31fa8090e9c00121e154fdfed6f5e0e170
Jonathan <Jkilach@gmail.com>
2023-12-21 17:50:47 +0000

    Techtribe initial commit

---
+++ techtribe.py
@@ -0,0 +1,8 @@
+name = "John"
+age = 30
+ 
+# Using the format() method for string formatting
+message = "Hello, my name is {} and I am {} years old.".format(name, age)
+ 
+# Print the formatted message
+print(message)
